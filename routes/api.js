10ef7fb4a04bd96e82bb6ad9113f8566af1fb9b2
addinakhair55 <addi21148ti@student.nurulfikri.ac.id>
2022-12-23 12:21:13 +0700

    Menambahkan file

---
+++ routes/api.js
@@ -0,0 +1,26 @@
+// Import studentController
+const StudentController = require("../controllers/StudentController");
+
+// Import express
+const express = require("express");
+
+// Membuat object express
+const router = express.Router();
+
+/**
+ * Membuat routing
+ */
+router.get("/",(req,res) => {
+    res.send("Hello Express JS");
+});
+
+router.get("/students", StudentController.index);
+
+router.post("/students", StudentController.store);
+
+// router.put("/students/:id", StudentController.update);
+
+// router.delete("/students/:id", StudentController.destroy);
+
+// Export router
+module.exports = router;
